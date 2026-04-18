ba6e8490c13b0f33105e910cf351db36c5f6d2c9
Алексей <ix.fenix@mail.ru>
2026-04-18 07:08:49 +0300

    feat: added utils

---
+++ Ludum Dare 59/Assets/_Source/GameAssembly/Utils/TempAudioSource.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public class TempAudioSource : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+
+        public void PlayAndDestroy(AudioClip clip = null)
+        {
+            if (!clip)
+            {
+                audioSource.Play();
+                Destroy(audioSource.gameObject, audioSource.clip.length);
+                return;
+            }
+            
+            audioSource.PlayOneShot(clip);
+            Destroy(audioSource.gameObject, clip.length);
+        }
+    }
+}
---
+++ Ludum Dare 59/Assets/_Source/GameAssembly/Utils/TempParticleSource.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public class TempParticleSource : MonoBehaviour
+    {
+        [SerializeField] public float destroyTime;
+
+        public void Init(float overrideDestroyTime = -1)
+        {
+            if(!Mathf.Approximately(overrideDestroyTime, -1))
+                destroyTime = overrideDestroyTime;
+        }
+
+        private void Start() => Destroy(gameObject, destroyTime);
+
+        private void OnEnable() => Start();
+    }
+}
