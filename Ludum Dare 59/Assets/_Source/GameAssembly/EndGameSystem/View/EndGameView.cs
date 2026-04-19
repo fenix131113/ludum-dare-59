37cd2738907c364ca236ac77d2b483f566222685
Алексей <ix.fenix@mail.ru>
2026-04-19 10:19:06 +0300

    feat: endgame view

---
+++ Ludum Dare 59/Assets/_Source/GameAssembly/EndGameSystem/View/EndGameView.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Player.Data;
+using Player.Variables;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Utils.VariablesSystem;
+using VContainer;
+
+namespace EndGameSystem.View
+{
+    public class EndGameView : MonoBehaviour
+    {
+        [SerializeField] private int menuSceneIndex = 1;
+        [SerializeField] private GameObject endPanel;
+        [SerializeField] private Button menuButton;
+        [SerializeField] private TMP_Text resultLabel;
+        [SerializeField] private string winCaption = "You win!";
+        [SerializeField] private string looseCaption = "You loose!";
+
+        [Inject] private EndGame _endGame;
+        [Inject] private IVariablesResolver<PlayerVariableBlockerType, Action, Action>  _variablesResolver;
+
+        private readonly PlayerVariableBlocker _endBlocker = new(PlayerVariableBlockerType.ALL);
+
+        private void Start() => Bind();
+
+        private void OnDestroy() => Expose();
+
+        private void OnGameEnded(bool isWin)
+        {
+            _variablesResolver.RegisterBlocker(_endBlocker);
+            
+            if(isWin)
+                Win();
+            else
+                Loose();
+            
+            endPanel.SetActive(true);
+        }
+
+        private void OnMenuButtonClicked()
+        {
+            SceneManager.LoadScene(menuSceneIndex);
+        }
+
+        private void Win()
+        {
+            resultLabel.text = winCaption;
+        }
+
+        private void Loose()
+        {
+            resultLabel.text = looseCaption;
+        }
+        
+        private void Bind()
+        {
+            _endGame.OnGameEnded += OnGameEnded;
+            menuButton.onClick.AddListener(OnMenuButtonClicked);
+        }
+
+        private void Expose()
+        {
+            _endGame.OnGameEnded -= OnGameEnded;
+            menuButton.onClick.RemoveAllListeners();
+        }
+    }
+}
