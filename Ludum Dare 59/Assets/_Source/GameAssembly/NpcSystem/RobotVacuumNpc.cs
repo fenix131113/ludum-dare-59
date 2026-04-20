fb3d90f73be64cb6ae19dcb26e82e0fc3177ba3d
Алексей <ix.fenix@mail.ru>
2026-04-20 22:58:55 +0300

    feat: vacuum robot animations

--- Ludum Dare 59/Assets/_Source/GameAssembly/NpcSystem/RobotVacuumNpc.cs
+++ Ludum Dare 59/Assets/_Source/GameAssembly/NpcSystem/RobotVacuumNpc.cs
@@ -1,7 +1,20 @@
+using UnityEngine;
+
 namespace NpcSystem
 {
     public class RobotVacuumNpc : BaseNpc
     {
-        protected override void Tick() => Patrol();
+        private static readonly int _moveX = Animator.StringToHash("MoveX");
+        private static readonly int _moveY = Animator.StringToHash("MoveY");
+        
+        [SerializeField] private Animator anim;
+        
+        protected override void Tick()
+        {
+            Patrol();
+            
+            anim.SetFloat(_moveX, aiPath.velocity.x);
+            anim.SetFloat(_moveY, aiPath.velocity.y);
+        }
     }
 }
